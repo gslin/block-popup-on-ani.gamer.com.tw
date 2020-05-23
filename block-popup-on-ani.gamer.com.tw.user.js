ad77620414d514fb35a6381f15b0b1a1bf6d8370
Gea-Suan Lin <gslin@gslin.org>
2020-05-23 16:30:30 +0800

    Import.

---
+++ block-popup-on-ani.gamer.com.tw.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Block popup on ani.gamer.com.tw
+// @namespace    https://github.com/gslin/block-popup-on-ani.gamer.com.tw/
+// @version      0.20200523.0
+// @description  Block popup on ani.gamer.com.tw
+// @author       Gea-Suan Lin <gslin@gslin.com>
+// @match        https://ani.gamer.com.tw/*
+// @grant        none
+// @license      MIT
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.alert = (msg) => {
+        console.debug('Alert (blocked): ' + msg)
+    };
+})();
